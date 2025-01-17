f74a8ce362e252d1288bc4309fa5c9ce6ae86d9c
Jeronimo <jerolambre@gmail.com>
2025-01-17 14:54:46 -0300

    Start Web

--- web/Users.Web/Program.cs
+++ web/Users.Web/Program.cs
@@ -1,10 +1,15 @@
 using Users.Web.Manegement.Components;
+using MudBlazor.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
 
 var app = builder.Build();
 
